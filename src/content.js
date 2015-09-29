ac0e168bf4c436f362b9e23688aafc158293e887
Sergey <sergeypanic@gmail.com>
2015-09-29 20:14:13 +0300

    select / deselect block with border

--- src/content.js
+++ src/content.js
@@ -1,5 +1,10 @@
 console.log('yo');
 
 $('body').click(function(event) {
-	alert($(event.target).text());
+	var elem = $(event.target);
+	if (elem.attr('selected_elem')) {
+		elem.css('border','none').removeAttr('selected_elem');
+	} else {
+		elem.css('border','2px solid red').attr('selected_elem','true');
+	}
 });
